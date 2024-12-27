8b79d10f2564c0449ddc12cc8743bf68938866f8
LuongND <nguyenluong132567@gmail.com>
2024-12-27 22:36:26 +0700

    update workflows

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,23 @@ const nextTranslate = require('next-translate-plugin');
 const nextConfig = {
   reactStrictMode: true,
   ...nextTranslate(),
-  transpilePackages: ['rc-util', '@ant-design', '@ant-design/icons', 'antd', 'rc-pagination', 'rc-picker', 'next-translate-plugin', 'next-translate']
+  transpilePackages: [
+    'rc-util',
+    '@ant-design',
+    '@ant-design/icons',
+    'antd',
+    'rc-pagination',
+    'rc-picker',
+    'next-translate-plugin',
+    'next-translate'
+  ],
+  
+  // Cấu hình để export static site
+  output: 'export',  // Thêm cấu hình này để Next.js xuất site tĩnh
+  
+  // Nếu bạn cần thêm cấu hình basePath hoặc assetPrefix cho GitHub Pages:
+  basePath: '/Nevel-Tech---Test',
+  assetPrefix: '/Nevel-Tech---Test',
 };
 
 export default nextConfig;
